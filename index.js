ee24ccc065f408f200f55c0f6fc34aee54f1f7d3
Lucas Teles <teleslucas@ymail.com>
2021-01-28 20:38:22 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const customExpress = require('./config/customExpress');
+const app = customExpress();
+app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+app.get('/', (req, res) => res.send('Servidor rodando, tudo ok'))
