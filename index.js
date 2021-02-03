5380229b1bdc90ccdb4fafb99196b319742eacd5
Lucas Teles <teleslucas@ymail.com>
2021-02-02 22:42:32 -0300

    Criando a conexão com mysql

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const customExpress = require('./config/customExpress');
+const customExpress = require("./config/customExpress");
 const app = customExpress();
-app.listen(3000, () => console.log('servidor rodando na porta 3000'));
-app.get('/', (req, res) => res.send('Servidor rodando, tudo ok'))
+const conexao = require("./infraestrutura/conexao");
+
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+    } else {
+        console.log("foi")
+        app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+    }
+});
+app.get("/", (req, res) => res.send("Servidor rodando, tudo ok"));
