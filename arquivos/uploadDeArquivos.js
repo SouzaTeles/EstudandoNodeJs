cd4659f34f70301ccf69788f29fce047b33dc7c9
Lucas  Teles <lucas.mkt@alterdata.com.br>
2021-02-27 09:53:18 -0300

    Trabalhando com stream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,5 @@
 const fs = require("fs");
 
-fs.readFile("./i.jpg", (erro, buffer) => {
-  if (erro) console.log(erro);
-//   else console.log("A imagem foi bufferizada\n" + buffer);
-
-  fs.writeFile("./assets/imagem.jpg", buffer, (erro) => {
-      if(!erro) console.log('a imagem foi escrita');
-  });
-});
+fs.createReadStream("./i.jpg")
+  .pipe(fs.createWriteStream("./assets/novaimagem.jpg"))
+  .on("finish", () => console.log("imagem foi escrita com sucesso"));
